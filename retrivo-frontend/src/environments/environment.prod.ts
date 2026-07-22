be1238e807fd5fc045d704487b1cb1c98073252e
Sumit <rautsunit2006@gmail.com>
2026-07-23 00:13:14 +0530

    Fix production environment file replacement and Google client ID

--- retrivo-frontend/src/environments/environment.prod.ts
+++ retrivo-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://retrivo-api-0dls.onrender.com/api'
+  apiUrl: 'https://retrivo-api-0dls.onrender.com/api',
+  googleClientId: '506912634476-l5llj1295dgl8vi0lc0mlhj1ai92f9m7.apps.googleusercontent.com'
 };
